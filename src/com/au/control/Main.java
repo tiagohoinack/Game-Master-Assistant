637788d2d7fe527c6829e78656f3a0681ca5422c
Tiago Hoinack <tiagohoinack@hotmail.com>
2016-11-03 23:54:25 -0200

    Correções iniciais
    Pequenas correções no projeto inicial

--- src/com/au/control/Main.java
+++ src/com/au/control/Main.java
@@ -1,17 +1,20 @@
-package Game Master Assistant;
-	
+package com.au.control;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			FlowPane root = new FlowPane();
 			Scene scene = new Scene(root,400,400);
+			Label textoTeste = new Label("Olá, Beta Tester do Game Master Assistant!");
+			root.getChildren().add(textoTeste);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
